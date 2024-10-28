00e91a542f41783c1aced5d0e06597c13e3c0e41
Zandex90 <prozas2018@udec.cl>
2024-10-27 22:21:20 -0300

    avance_I y J

---
+++ matlab/problema_Ic.m
@@ -0,0 +1,27 @@
+matrices;
+T = 200e-3;
+
+%% Ganancias del sistema
+k_st = 180/pi; k_a = 100; k_c = 4e-3;
+
+%% Lugar Geometrico de las Raices
+sys_c = ss(A, B, C, 0);
+
+z = tf('z', T);
+sys = c2d(sys_c, T);
+ret = 1/z; % Retardo por calculo
+LaD = k_c * ret * k_a * sys * k_st; % Lazo Directo
+aux_LaD =  ret * k_c * k_a * sys; 
+LaC = feedback(aux_LaD, k_st);  % Lazo Cerrado
+
+%% Determinacion polos y ceros
+[p_LaD, z_LaD] = pzmap(LaD);  % Lazo Directo
+[p_LaC, z_LaC] = pzmap(LaC);  % Lazo Cerrado
+disp('Polos del L.D:');
+disp(p_LaD);
+disp('Polos del L.C:');
+disp(p_LaC);
+disp('ceros del L.D:');
+disp(z_LaD);
+disp('ceros del L.C:');
+disp(z_LaC);
