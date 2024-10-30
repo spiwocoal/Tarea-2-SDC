63bd2bdf795ffd349a51e1f56a2da56bf5cf6347
Spiwocoal <spiwocoal@gmail.com>
2024-10-29 21:23:57 -0300

    Comentarios I) J)

--- matlab/problema_he.m
+++ matlab/problema_he.m
@@ -15,11 +15,11 @@ LaD = (1/st) * k_st * k_a * sys; % Lazo Directo
 LaD_syms = poly2sym(num,s)/poly2sym(den,s);
 
 eq1 = 1 + kc * LaD_syms;
-polos = solve(eq2 == 0, s, MaxDegree=4);
+polos = solve(eq1 == 0, s, MaxDegree=4);
 
 eq2 = real(polos(1)) == 10 * real(polos(3));
 
-kc_orden1 = solve(eq2, kc);
+kc_orden1 = vpasolve(eq2, kc);
 
 fprintf('Ganancia para sistema de orden 1: %.4f\n', ...
     kc_orden1);
